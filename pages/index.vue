91a845aec4893bfc1e325e409000ce11bb68d58e
DarshGupta16 <somefakeemail@gmail.com>
2020-11-06 22:55:49 +0530

    Adding some of the interactivity

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
     <h1>My Todos</h1>
+    <form @submit.prevent="onSubmit">
+      <input v-model="todoInput" type="text" />
+      <input type="submit" value="Add Todo" />
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 export default {
+  data() {
+    return {
+      todos: ['My Todo List', 'Always remember to finish all your work'],
+      todoInput: '',
+    }
+  },
+  mounted() {
+    this.todos = JSON.parse(localStorage.getItem('todos')!)
+  },
+  methods: {
+    onSubmit() {
+      this.todos.push(this.todoInput)
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+      this.todoInput = ''
+    },
+  },
   head() {
     return {
       title: 'My Todos',
