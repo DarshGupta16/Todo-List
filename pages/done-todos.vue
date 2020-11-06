34f574741eff5914a46907e78b4399d613d095df
DarshGupta16 <somefakeemail@gmail.com>
2020-11-06 20:36:43 +0530

    Renaming done.vue to done-todos.vue

---
+++ pages/done-todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Done Todos</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  head() {
+    return {
+      title: 'Done Todos',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'All the todos you have done.',
+        },
+      ],
+    }
+  },
+}
+</script>
