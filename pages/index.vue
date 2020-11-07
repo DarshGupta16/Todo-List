1be0f7f4fcc5eff53b086757eea998516c2a81fd
DarshGupta16 <somefakeemail@gmail.com>
2020-11-07 19:01:07 +0530

    Adding some styles to the todo textbox

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,33 @@
   <div>
     <h1>My Todos</h1>
     <form @submit.prevent="onSubmit">
-      <input v-model="todoInput" type="text" />
-      <input type="submit" value="Add Todo" />
+      <input v-model="todoInput" type="text" class="input" />
+      <input type="submit" value="Add Todo" class="btn" />
     </form>
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+.input {
+  font-size: 30px;
+  border: 1px solid black;
+  border-radius: none;
+  outline: none;
+  width: 500px;
+}
+
+.btn {
+  font-size: 30px;
+  border: none;
+  border-radius: none;
+  outline: none;
+  background-color: #52de97;
+  font-family: 'Montserrat', sans-serif;
+  cursor: pointer;
+}
+</style>
 
 <script lang="ts">
 export default {
